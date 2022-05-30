34db56330382a7f942cc667de0981d14416cfc7b
Kirill Konstantinov <me@st8ed.com>
2022-05-30 10:30:02 +0300

    Extract overlay to images.nix

--- flake.nix
+++ flake.nix
@@ -13,39 +13,6 @@
         inherit system;
         overlays = [ self.overlay ];
       });
-
-      overlay = pkgs: super: {
-        haproxy-ingress = pkgs.callPackage ./pkgs/haproxy-ingress.nix { };
-
-        gitea-docker-entrypoint = pkgs.callPackage ./pkgs/gitea-docker-entrypoint.nix {
-          gitea = pkgs.gitea.override {
-            pamSupport = false;
-          };
-        };
-
-        kube-state-metrics = pkgs.callPackage ./pkgs/kube-state-metrics.nix { };
-        prometheus-operator = pkgs.callPackage ./pkgs/prometheus-operator.nix { };
-
-        dockerImages = pkgs.lib.makeScope super.newScope (self: {
-          busybox = pkgs.callPackage ./images/busybox.nix { };
-          code-server = pkgs.callPackage ./images/code-server.nix { };
-          docker-registry = pkgs.callPackage ./images/docker-registry.nix { };
-          gitea = pkgs.callPackage ./images/gitea.nix { };
-          prometheus = pkgs.callPackage ./images/prometheus.nix { };
-          prometheus-operator = pkgs.callPackage ./images/prometheus-operator.nix { };
-          prometheus-alertmanager = pkgs.callPackage ./images/prometheus-alertmanager.nix { };
-          prometheus-config-reloader = pkgs.callPackage ./images/prometheus-config-reloader.nix { };
-          prometheus-admission-webhook = pkgs.callPackage ./images/prometheus-admission-webhook.nix { };
-          grafana = pkgs.callPackage ./images/grafana.nix { };
-          haproxy-ingress = pkgs.callPackage ./images/haproxy-ingress.nix { };
-          haproxy = pkgs.callPackage ./images/haproxy.nix { };
-          home-assistant = pkgs.callPackage ./images/home-assistant.nix { };
-          k8s-sidecar = pkgs.callPackage ./images/k8s-sidecar.nix { };
-          kube-state-metrics = pkgs.callPackage ./images/kube-state-metrics.nix { };
-          minio = pkgs.callPackage ./images/minio.nix { };
-          mopidy = pkgs.callPackage ./images/mopidy.nix { };
-        });
-      };
     in
     {
       packages = forAllSystems (system: nixpkgsFor."${system}".dockerImages);
@@ -100,7 +67,7 @@
             });
           });
         })
-        overlay
+        (import ./images.nix)
       ];
     };
 }
